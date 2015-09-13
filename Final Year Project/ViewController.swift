bb776308a2dd0232a43a60e7ff4476fa5930476f
wlfzxq <wlfzxq@sina.com>
2015-09-13 19:30:56 +0800

    Initial Commit

---
+++ Final Year Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Final Year Project
+//
+//  Created by Wang Longfei on /139/15.
+//  Copyright (c) 2015 Wang Longfei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Final Year ProjectTests/Final_Year_ProjectTests.swift
@@ -0,0 +1,36 @@
+//
+//  Final_Year_ProjectTests.swift
+//  Final Year ProjectTests
+//
+//  Created by Wang Longfei on /139/15.
+//  Copyright (c) 2015 Wang Longfei. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Final_Year_ProjectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
