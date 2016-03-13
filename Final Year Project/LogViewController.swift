cc704b0a2bc9c6e6308ed683985b6a6c8ee479fc
#WANG0780# <wang0780@e.ntu.edu.sg>
2016-03-14 01:03:38 +0800

    Mar12 with charts

--- Final Year Project/LogViewController.swift
+++ Final Year Project/LogViewController.swift
@@ -8,15 +8,26 @@
 
 import UIKit
 import WatchConnectivity
+import Charts
 
-class LogViewController: UIViewController{
+class LogViewController: UIViewController,UIScrollViewDelegate{
 
-    @IBOutlet weak var seg: UISegmentedControl!
-    @IBOutlet weak var test: UILabel!
+//    @IBOutlet weak var barChartView: BarChartView!
+   
+    @IBOutlet weak var pieChartView: PieChartView!
+    @IBOutlet weak var lineChartView: LineChartView!
+   // @IBOutlet weak var test: UILabel!
+    @IBOutlet var scrollView: UIScrollView!
 
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
+        
+        setChart(months, values: unitsSold)
+
+        
         
         // Do any additional setup after loading the view.
     }
@@ -30,9 +41,46 @@ class LogViewController: UIViewController{
     
     
     
+    var months:[String]!
 
+    func setChart(dataPoints: [String], values: [Double]) {
+//        barChartView.noDataText = "You need to provide data for the chart."
+        
+        var dataEntries: [ChartDataEntry] = []
+        
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry = ChartDataEntry(value: values[i], xIndex: i)
 
-
+            dataEntries.append(dataEntry)
+        }
+        let pieChartDataSet = PieChartDataSet(yVals: dataEntries, label: "Units Sold")
+        let pieChartData = PieChartData(xVals: dataPoints, dataSet: pieChartDataSet)
+        pieChartView.data = pieChartData
+        
+        var colors: [UIColor] = []
+        
+        for i in 0..<dataPoints.count {
+            let red = Double(arc4random_uniform(256))
+            let green = Double(arc4random_uniform(256))
+            let blue = Double(arc4random_uniform(256))
+            
+            let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+            colors.append(color)
+        }
+        
+        pieChartDataSet.colors = colors
+//        let chartDataSet = BarChartDataSet(yVals: dataEntries, label: "Units Sold")
+//        let chartData = BarChartData(xVals: months, dataSet: chartDataSet)
+//        barChartView.data = chartData
+        print("1")
+        let lineChartDataSet = LineChartDataSet(yVals: dataEntries, label: "Units Sold")
+        print("2")
+        let lineChartData = LineChartData(xVals: dataPoints, dataSet: lineChartDataSet)
+        print("3")
+        lineChartView.data = lineChartData
+        
+    }
 
 
     /*
